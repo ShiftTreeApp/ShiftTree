3c3e88da79c15c48701fb6e9bc54fcd2f36a1fda
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-12 20:38:14 -0700

    wire up sign in page

--- client/src/SignIn.tsx
+++ client/src/SignIn.tsx
@@ -1,42 +1,58 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import { useMemo, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid2";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useApi } from "@/client";
+import { useNavigate, useParams } from "react-router";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
+export type SignInParams = {
+  from: string;
+};
+
 export default function SignIn() {
+  const api = useApi();
+  const navigate = useNavigate();
+  const routeParams = useParams<SignInParams>();
+
+  const { mutateAsync: submitLogin, error: loginError } = api.useMutation(
+    "post",
+    "/login",
+  );
+
+  function onLogin(accessToken: string) {
+    console.table({ accessToken });
+    localStorage.setItem("accessToken", accessToken);
+    navigate(routeParams.from ?? "/");
+  }
+
+  const [remember, setRemember] = useState(false);
+
+  // TODO: Ensure server error messages are good enough to show to the user
+  const errorMessage = useMemo(() => loginError?.toString(), [loginError]);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const email = data.get("email") as string;
+    const password = data.get("password") as string;
+    // TODO: Disable logging password in production
+    console.table({ email, password });
+    submitLogin({ body: { email, password } }).then(({ accessToken }) =>
+      onLogin(accessToken),
+    );
   };
 
   return (
@@ -46,20 +62,27 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Grid
+            container
+            spacing={2}
+            direction="column"
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 4 }}
+          >
             <TextField
-              margin="normal"
               required
               fullWidth
               id="email"
@@ -69,7 +92,6 @@ export default function SignIn() {
               autoFocus
             />
             <TextField
-              margin="normal"
               required
               fullWidth
               name="password"
@@ -79,32 +101,42 @@ export default function SignIn() {
               autoComplete="current-password"
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
+              control={
+                <Checkbox
+                  onChange={e => setRemember(e.target.checked)}
+                  checked={remember}
+                  value="remember"
+                  color="primary"
+                />
+              }
               label="Remember me"
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
+            <Button type="submit" fullWidth variant="contained">
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
+            <Box>
+              {errorMessage && (
+                <Typography color="error">{errorMessage}</Typography>
+              )}
+            </Box>
+            <Grid
+              container
+              spacing={2}
+              sx={{ justifyContent: "space-between" }}
+            >
+              <Grid>
+                <Link href="/reset" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
+              <Grid>
+                <Link href="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
