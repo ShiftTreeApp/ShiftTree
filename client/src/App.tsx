81a1d37591f59e0219e1c040dce50e63730a5bc3
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-12 18:43:44 -0700

    wrap router with ApiProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import * as React from "react";
 import SignIn from "./SignIn.tsx";
 import SignUp from "./SignUp.tsx";
+import { ApiProvider } from "@/client.tsx";
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp/>}/>
-      </Routes>
-    </BrowserRouter>
+    <ApiProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </ApiProvider>
   );
 }
