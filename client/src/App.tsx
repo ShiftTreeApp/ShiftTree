78a11f1554043a5391e06e80565125828457b025
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-17 18:45:44 -0700

    remove mytrees and rename JoinTree to join

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { ApiProvider } from "@/client.tsx";
 import { Authenticated, AuthProvider } from "@/auth";
 import Home from "@/Home.tsx";
 import Create from "@/Create.tsx";
-import MyTrees from "@/MyTrees.tsx";
 import JoinTree from "@/JoinTree.tsx";
 
 export default function App() {
@@ -36,7 +35,7 @@ export default function App() {
               }
             />
             <Route
-              path="/JoinTree"
+              path="/join"
               element={
                 <Authenticated>
                   <JoinTree />
