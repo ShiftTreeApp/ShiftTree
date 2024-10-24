3681f5efa3dede43bcc2a75f4a2ce8ff19cca99a
santi <saameza@ucsc.edu>
2024-10-24 10:31:11 -0700

    added icons to navbar, non-functional

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useNavigate } from "react-router";
 import { Link as RouterLink } from "react-router-dom";
 import {
@@ -8,7 +9,13 @@ import {
   Link,
   Toolbar,
   Typography,
+  IconButton,
+  Menu,
+  MenuItem,
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 import { useAuth } from "@/auth";
 
@@ -22,6 +29,15 @@ export default function Navbar() {
     navigate("/");
   }
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Grid container>
       <Box sx={{ flexGrow: 1 }}>
@@ -34,9 +50,43 @@ export default function Navbar() {
                 ShiftTree
               </Typography>
             </Link>
-            <Button color="inherit" onClick={onLogout}>
-              Log out
-            </Button>
+
+            <Grid>
+              <IconButton
+                id="add-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                aria-label="add"
+                size="small"
+              >
+                <AddIcon fontSize="medium" />
+              </IconButton>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "add-button",
+                }}
+              >
+                <MenuItem onClick={handleClose}>Create ShiftTree</MenuItem>
+                <MenuItem onClick={handleClose}>Create Organization</MenuItem>
+                <MenuItem onClick={handleClose}>Join ShiftTree</MenuItem>
+                <MenuItem onClick={handleClose}>Join Organization</MenuItem>
+              </Menu>
+              <IconButton aria-label="notif" size="small">
+                <NotificationsIcon fontSize="medium" />
+              </IconButton>
+              <IconButton aria-label="profile" size="small">
+                <AccountCircleIcon fontSize="medium" />
+              </IconButton>
+              <Button color="inherit" onClick={onLogout}>
+                Log out
+              </Button>
+            </Grid>
           </Toolbar>
         </AppBar>
       </Box>
