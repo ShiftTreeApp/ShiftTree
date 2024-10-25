c93e229e5ec7cf2cbc30b309ee530a7c36bc7227
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-24 23:08:06 -0700

    add checks for env vars in pool creation

--- server/src/pool.ts
+++ server/src/pool.ts
@@ -1,9 +1,35 @@
 import { Pool } from "pg";
 
+const host = process.env.SHIFTTREE_PG_HOST;
+if (!host) {
+  throw new Error("Environment variable SHIFTTREE_PG_HOST is not set");
+}
+
+const port0 = process.env.SHIFTTREE_PG_PORT;
+if (!port0) {
+  throw new Error("Environment variable SHIFTTREE_PG_PORT is not set");
+}
+const port = parseInt(port0);
+
+const database = process.env.SHIFTTREE_PG_DATABASE;
+if (!database) {
+  throw new Error("Environment variable SHIFTTREE_PG_DATABASE is not set");
+}
+
+const user = process.env.SHIFTTREE_PG_USER;
+if (!user) {
+  throw new Error("Environment variable SHIFTTREE_PG_USER is not set");
+}
+
+const password = process.env.SHIFTTREE_PG_PASSWORD;
+if (!password) {
+  throw new Error("Environment variable SHIFTTREE_PG_PASSWORD is not set");
+}
+
 export const pool = new Pool({
-  host: process.env.SHIFTTREE_PG_HOST,
-  port: parseInt(process.env.SHIFTTREE_PG_PORT ?? "5432"),
-  database: process.env.SHIFTTREE_PG_DATABASE,
-  user: process.env.SHIFTTREE_PG_USER,
-  password: process.env.SHIFTTREE_PG_PASSWORD,
+  host,
+  port,
+  database,
+  user,
+  password,
 });
