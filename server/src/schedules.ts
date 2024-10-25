4eae63362496e26e6470350bc32d65c1e71ec0e9
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-24 23:00:13 -0700

    fix linter errors in schedule

--- server/src/schedules.ts
+++ server/src/schedules.ts
@@ -4,38 +4,30 @@ import * as jwt from "jsonwebtoken";
 
 import { pool } from "@/pool";
 
-const createScheduleInfo = z.object({
-  name: z.string(),
-  owner: z.string().optional(),
-  description: z.string().optional(),
-});
-
 const tokenPayload = z.object({ email: z.string(), name: z.string() });
 
 export async function create(req: Request, res: Response) {
-  // TODO: Come up with a better way of doing this
-  const token = req.headers.authorization?.split(" ")[1]!;
-  const decoded = tokenPayload.parse(
-    jwt.verify(token, process.env.SHIFTTREE_JWT_PK as string),
-  );
+  const userId = await getUserId(req);
 
-  const scheduleInfo = createScheduleInfo.parse(req.body);
-  const query = `
-    WITH owner AS (
-      SELECT u.id
-      FROM user_account AS u
-      WHERE u.email = $1
-    )
-    INSERT INTO schedule (owner_id, schedule_name)
-    SELECT o.id, $2
-    FROM owner AS o
-    RETURNING id as schedule_id
-  `;
+  const scheduleInfo = req.body as {
+    name: string;
+    description?: string | null;
+  };
+
   const result = await pool.query({
-    text: query,
-    values: [decoded.email, scheduleInfo.name],
+    text: /* sql */ `
+      insert into schedule (owner_id, schedule_name, schedule_description)
+      values ($1, $2, $3)
+      returning id
+    `,
+    values: [userId, scheduleInfo.name, scheduleInfo.description],
   });
-  const scheduleId = result.rows[0].schedule_id as string;
+  if (result.rows.length < 1) {
+    console.error("Failed to create schedule");
+    res.status(500).json({ error: "Internal Server Error" });
+    return;
+  }
+  const scheduleId = result.rows[0].id as string;
   res.status(201).json({ scheduleId });
 }
 
@@ -44,7 +36,10 @@ const listParams = z.object({
 });
 
 async function getUserId(req: Request) {
-  const token = req.headers.authorization?.split(" ")[1]!;
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    throw new Error("Missing token");
+  }
   const decoded = tokenPayload.parse(
     jwt.verify(token, process.env.SHIFTTREE_JWT_PK as string),
   );
