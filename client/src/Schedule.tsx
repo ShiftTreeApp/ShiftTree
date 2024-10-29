6e2dfbbaff3da43a177ee96dc57c6d7668e11006
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-29 14:11:29 -0700

    scaffold for viewing page

--- client/src/Schedule.tsx
+++ client/src/Schedule.tsx
@@ -1,3 +1,15 @@
+import Navbar from "@/Navbar";
+import NavbarPadding from "@/NavbarPadding";
+import { Container, Grid2 as Grid, Typography } from "@mui/material";
+
 export default function Schedule() {
-  return <></>;
+  return (
+    <Grid container direction="column" spacing={1}>
+      <Navbar />
+      <NavbarPadding />
+      <Container component="main" sx={{}}>
+        <Typography>Content</Typography>
+      </Container>
+    </Grid>
+  );
 }
