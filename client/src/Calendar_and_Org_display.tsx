c3ff307099da464bd48ff0f67c63445e623b9d90
santi <saameza@ucsc.edu>
2024-10-29 18:51:09 -0700

    changed calendar from skeleton to one with functionality

--- client/src/Calendar_and_Org_display.tsx
+++ client/src/Calendar_and_Org_display.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   //Button,
   Grid2 as Grid,
@@ -8,10 +9,20 @@ import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import "dayjs/locale/en";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import Org_display from "./Org_display";
 
 export default function Calendar_and_Org() {
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+
+  const handleDateChange = (date: Dayjs | null) => {
+    setSelectedDate(date);
+    if (date) {
+      console.log(date.format("YYYY-MM-DD"));
+    }
+  };
+
   return (
     <Grid sx={{ flexGrow: 1, display: "flex" }}>
       <Paper
@@ -24,7 +35,11 @@ export default function Calendar_and_Org() {
       >
         {/* Weird Calendar Shit */}
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar renderLoading={() => <DayCalendarSkeleton />} />
+          <DateCalendar
+            renderLoading={() => <DayCalendarSkeleton />}
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
         </LocalizationProvider>
 
         {/* Grid that contains Organizations */}
