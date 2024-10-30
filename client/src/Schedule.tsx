434542119fe7eec995abc3eacc1613289c1c04dd
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-10-30 15:56:27 -0700

    add drawer for viewing shift details

--- client/src/Schedule.tsx
+++ client/src/Schedule.tsx
@@ -3,29 +3,99 @@ import {
   Card,
   Chip,
   Container,
+  Drawer,
   Grid2 as Grid,
+  IconButton,
   Typography,
 } from "@mui/material";
+import { Close as CloseIcon } from "@mui/icons-material";
 import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
 import dayjs from "dayjs";
 
 import Navbar from "@/Navbar";
 import NavbarPadding from "@/NavbarPadding";
 
+function useSelectedShiftParam() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const selectedShift = searchParams.get("shift");
+  function setSelectedShift(shiftId: string) {
+    setSearchParams(prev => {
+      prev.set("shift", shiftId);
+      return prev;
+    });
+  }
+  function clearSelectedShift() {
+    setSearchParams(prev => {
+      prev.delete("shift");
+      return prev;
+    });
+  }
+
+  return { selectedShift, setSelectedShift, clearSelectedShift };
+}
+
 export default function Schedule() {
+  const { selectedShift, setSelectedShift, clearSelectedShift } =
+    useSelectedShiftParam();
+
+  const drawerOpen = selectedShift !== null;
+
   return (
     <Grid container direction="column" spacing={1}>
       <Navbar />
       <NavbarPadding />
+      <Drawer
+        open={drawerOpen}
+        onClose={clearSelectedShift}
+        anchor="right"
+        sx={theme => ({
+          ".MuiDrawer-paper": {
+            [theme.breakpoints.down("md")]: {
+              width: "100%",
+            },
+          },
+        })}
+      >
+        <NavbarPadding />
+        <Box
+          sx={theme => ({
+            [theme.breakpoints.up("md")]: {
+              width: theme => theme.spacing(96),
+            },
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            padding: 2,
+          })}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h6">View shift</Typography>
+            <IconButton onClick={clearSelectedShift}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <Typography variant="body1">Shift details</Typography>
+          <Typography variant="body1">Shift id: {selectedShift}</Typography>
+        </Box>
+      </Drawer>
       <Container
         component="main"
         sx={{ display: "flex", flexDirection: "column", alignItems: "stretch" }}
       >
         <ShiftCalendar
-          onClickShift={shiftId => console.log(shiftId)}
+          onClickShift={shiftId => setSelectedShift(shiftId)}
           startDate={dayjs("2024-10-27, 8:00")}
           endDate={dayjs("2024-11-20, 3:00")}
-          selectedShifts={["3"]}
+          selectedShifts={selectedShift ? [selectedShift] : []}
           shifts={[
             {
               id: "1",
