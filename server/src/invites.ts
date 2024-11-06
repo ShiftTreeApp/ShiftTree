e622085569b31602203fe4621dd0545bbeb18300
Noah Meisel <nmeisel@ucsc.edu>
2024-11-05 18:46:28 -0800

    fixed bug in backend

--- server/src/invites.ts
+++ server/src/invites.ts
@@ -73,18 +73,20 @@ export const generateJoinCode = async (req: Request, res: Response) => {
 export const joinShiftTree = async (req: Request, res: Response) => {
   try {
     const { JoinCode } = req.query;
+    console.log(JoinCode);
     const findScheduleQuery = {
       text: "SELECT id FROM schedule WHERE code = $1",
       values: [JoinCode],
     };
     const scheduleResult = await pool.query(findScheduleQuery);
-    const UserID = getUserId(req);
+    const UserID = await getUserId(req);
     if (scheduleResult.rows.length === 0) {
       return res.status(404).send("Invalid join code");
     }
 
     const scheduleID = scheduleResult.rows[0].id;
-
+    console.log(scheduleID);
+    console.log(UserID);
     const statement = `
       INSERT INTO user_schedule_membership (id, user_id, schedule_id)
       VALUES (gen_random_uuid(), $1, $2)
