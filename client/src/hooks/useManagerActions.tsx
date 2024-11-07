75ed927c1307a49a36d0d92f2199a10e47436321
AQUAiTE <francisiwnlschool@gmail.com>
2024-11-06 22:06:15 -0800

    Hook function for getting existing code

--- client/src/hooks/useManagerActions.tsx
+++ client/src/hooks/useManagerActions.tsx
@@ -1,6 +1,6 @@
 import { useApi } from "@/client";
 
-export function useManagerActions() {
+export function useManagerActions(shiftTreeId: string) {
   const api = useApi();
 
   const { mutateAsync: generateCode } = api.useMutation(
@@ -8,21 +8,38 @@ export function useManagerActions() {
     "/shiftTreeCodeGenerate",
   );
 
-  return {
-    async generate({ shiftTreeId }: { shiftTreeId: string }): Promise<void> {
-      const { code } = await generateCode({
-        params: {
-          query: {
-            ShiftTreeID: shiftTreeId,
-          },
+  async function generate({
+    shiftTreeId,
+  }: {
+    shiftTreeId: string;
+  }): Promise<void> {
+    const { code } = await generateCode({
+      params: {
+        query: {
+          ShiftTreeID: shiftTreeId,
         },
-        /* TODO: Use this/replace this with manager authentication
+      },
+      /* TODO: Use this/replace this with manager authentication
         headers: {
           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },*/
-        // TODO: Do something with the code var
-      });
-      console.log("Invite Code Generated");
+      // TODO: Do something with the code var
+    });
+    console.log("Invite Code Generated");
+  }
+
+  const { data } = api.useQuery("get", "/shiftTreeCodeExisting", {
+    params: {
+      query: {
+        ShiftTreeID: shiftTreeId,
+      },
     },
-  };
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
+
+  const existingCode = data?.code;
+
+  return { generate, existingCode };
 }
