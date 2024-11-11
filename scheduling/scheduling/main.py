d226b9b3eef41570443ceecb47ae1613a2dfbd52
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-11-11 13:39:15 -0800

    add fastapi server

--- scheduling/scheduling/main.py
+++ scheduling/scheduling/main.py
@@ -1,5 +1,29 @@
+import os
+
+import dotenv
+import fastapi
+
+from scheduling.models import Config, ScheduleRequest, ScheduleResponse
+
+app = fastapi.FastAPI()
+
+
+@app.get("/shifts")
+def generate_schedule(_req: ScheduleRequest) -> ScheduleResponse:
+    return ScheduleResponse(assignments=[])
+
+
 def main():
-    print("Hello, World")
+    import uvicorn
+
+    config = Config(
+        **{
+            **dotenv.dotenv_values(".env"),
+            **os.environ,
+        }
+    )
+
+    uvicorn.run(app, host=config.host, port=config.port)
 
 
 if __name__ == "__main__":
---
+++ scheduling/scheduling/models.py
@@ -0,0 +1,40 @@
+from collections.abc import Sequence
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+type ShiftId = str
+
+
+type UserId = str
+
+
+class Signup(BaseModel):
+    weight: float
+    user_id: UserId
+
+
+class Shift(BaseModel):
+    id: ShiftId
+    start_time: datetime
+    end_time: datetime
+    signups: Sequence[Signup]
+
+
+class ScheduleRequest(BaseModel):
+    shifts: Sequence[Shift]
+
+
+class Assignment(BaseModel):
+    shift_id: ShiftId
+    user_id: UserId
+    requested_weight: float | None
+
+
+class ScheduleResponse(BaseModel):
+    assignments: Sequence[Assignment]
+
+
+class Config(BaseModel):
+    host: str = Field(default="::", validation_alias="SHIFTTREE_SCHEDULING_HOST")
+    port: int = Field(default=8080, validation_alias="SHIFTTREE_SCHEDULING_PORT")
