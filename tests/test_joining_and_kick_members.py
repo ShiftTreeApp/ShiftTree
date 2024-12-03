7401eab92f38d7f4b354f6f8fd56ca3a83c0eae8
Evan Jones (原文轩) <48502664+yuenmh@users.noreply.github.com>
2024-12-02 17:11:27 -0800

    More tests

--- tests/test_joining_and_kick_members.py
+++ tests/test_joining_and_kick_members.py
@@ -1,4 +1,6 @@
+import asyncio
 from collections.abc import Sequence
+from uuid import UUID
 import utils
 import pytest
 
@@ -169,3 +171,84 @@ async def test_only_owner_allowed_to_see_members():
         schedule_id=schedule_id,
     )
     assert isinstance(members, Sequence)
+
+
+@pytest.mark.asyncio
+async def test_unable_to_join_with_old_code():
+    owner, _ = await utils.create_account_and_login()
+    user, _ = await utils.create_account_and_login()
+
+    schedule = await post_schedules.asyncio(
+        client=owner,
+        body=post_schedules_body.PostSchedulesBody(name="New Schedule"),
+    )
+    assert schedule is not None
+
+    old_invite_code = await get_shift_tree_code_existing.asyncio(
+        client=owner,
+        shift_tree_id=str(schedule.schedule_id),
+    )
+    assert old_invite_code is not None
+
+    await get_shift_tree_code_generate.asyncio(
+        client=owner,
+        shift_tree_id=str(schedule.schedule_id),
+    )
+
+    join_res = await put_join_shift_tree.asyncio_detailed(
+        client=user,
+        join_code=old_invite_code.code,
+    )
+
+    assert join_res.status_code.value == 404
+
+
+@pytest.mark.asyncio
+async def test_kick_members():
+    owner, _ = await utils.create_account_and_login()
+    user1, user1_email = await utils.create_account_and_login()
+    user2, user2_email = await utils.create_account_and_login()
+
+    schedule = await post_schedules.asyncio(
+        client=owner,
+        body=post_schedules_body.PostSchedulesBody(name="New Schedule"),
+    )
+    assert schedule is not None
+
+    invite_code = await get_shift_tree_code_existing.asyncio(
+        client=owner,
+        shift_tree_id=str(schedule.schedule_id),
+    )
+    assert invite_code is not None
+
+    await put_join_shift_tree.asyncio_detailed(
+        client=user1,
+        join_code=invite_code.code,
+    )
+    await put_join_shift_tree.asyncio_detailed(
+        client=user2,
+        join_code=invite_code.code,
+    )
+
+    members = await get_schedules_schedule_id_members.asyncio(
+        client=owner,
+        schedule_id=schedule.schedule_id,
+    )
+    assert isinstance(members, Sequence)
+    assert {m.email for m in members} == {user1_email, user2_email}
+
+    async def remove(user_id: UUID):
+        await delete_remove_user_schedule_id.asyncio_detailed(
+            client=owner,
+            schedule_id=schedule.schedule_id,
+            user_id=user_id,
+        )
+
+    await asyncio.gather(*(remove(m.id) for m in members))
+
+    members = await get_schedules_schedule_id_members.asyncio(
+        client=owner,
+        schedule_id=schedule.schedule_id,
+    )
+    assert isinstance(members, Sequence)
+    assert {m.email for m in members} == set()
