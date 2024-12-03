8548082b9467d907964596619e1fa6b46e81db1a
AQUAiTE <francisiwnlschool@gmail.com>
2024-12-03 13:17:59 -0800

    Fixing icons

--- client/src/PasswordReset.tsx
+++ client/src/PasswordReset.tsx
@@ -63,7 +63,7 @@ export default function PasswordReset() {
       >
         <Avatar
           sx={{ m: 2, bgcolor: "primary.main", width: 64, height: 64 }}
-          src="https://github.com/ShiftTreeApp/ShiftTree/blob/main/shiftTreeImages/shiftSprout_avatar.png?raw=true"
+          src="https://github.com/ShiftTreeApp/ShiftTree/blob/main/icons/shiftSprout_avatar.png?raw=true"
         />
         <Typography component="h1" variant="h5">
           Enter new password
