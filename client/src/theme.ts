dc31800ffd2033ebc1dc17a453f4dee8b09d4285
AQUAiTE <francisiwnlschool@gmail.com>
2024-12-02 21:46:20 -0800

    Labeled theme colors

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -27,30 +27,30 @@ declare module "@mui/material/styles" {
 const customTheme = createTheme({
   palette: {
     primary: {
-      main: "#2E7D32", // #008b00
-      light: "#abcbad",
-      veryLight: "#f1f8e9",
+      main: "#2E7D32", // Medium Green
+      light: "#abcbad", // Pale Green
+      veryLight: "#f1f8e9", // Offwhite
     },
     secondary: {
-      main: "#ffa000",
-      light: "#ffca70",
-      veryLight: "#ffe082",
-      light2: "#ffecb0", // used for shift cards for better contrast
+      main: "#ffa000", // Light Orange
+      light: "#ffca70", // Pale Orange
+      veryLight: "#ffe082", // Pale Orange-Yellow
+      light2: "#ffecb0", // Orange Tint Offwhite
     },
     info: {
-      main: "#58A4B0",
-      light: "#527B6A", // used for green buttons that are not primary
-      light2: "#9BC1BC",
-      dark: "#A491D3", // purple used for generate shifts button
+      main: "#58A4B0", // Medium Cyan
+      light: "#527B6A", // Turquoise
+      light2: "#9BC1BC", // Pale Cyan
+      dark: "#A491D3", // Pale Purple
     },
     error: {
-      main: "#f26868",
-      light: "#f58686",
-      veryLight: "#f8d7da",
-      dark: "#C20114", // #F40000
+      main: "#f26868", // Pale Red
+      light: "#f58686", // Paler Red
+      veryLight: "#f8d7da", // Pink Tint Offwhite
+      dark: "#C20114", // Deep Red
     },
     background: {
-      default: "#e0ebc9", // Default background
+      default: "#e0ebc9", // Green Tint Offwhite
       paper: "#f3f7e2", // Background for Paper components (lighter shade)
     },
   },
